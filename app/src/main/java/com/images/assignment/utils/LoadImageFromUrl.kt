73dadb622aa215d81f92ec97b571ccc8c019e36c
Amarjit <amarjit@192.168.1.4>
2024-04-19 17:01:10 +0530

    changes in placeholder

--- app/src/main/java/com/images/assignment/utils/LoadImageFromUrl.kt
+++ app/src/main/java/com/images/assignment/utils/LoadImageFromUrl.kt
@@ -5,16 +5,23 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.google.android.engage.common.datamodel.Image
 import com.images.assignment.data.UnsplashImageResponse
 import com.images.assignment.database.ImageEntity
+import com.images.imagessmaple.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -50,13 +57,26 @@ fun ImageFromBase64String(base64EncodedImageString: String) {
         bitmap = BitmapFactory.decodeByteArray(decodedImageBytes, 0, decodedImageBytes.size)
 
     }
-
-    bitmap?.let {
-        Image(bitmap = it.asImageBitmap(), contentDescription = null)
+    if (bitmap == null)
+        Placeholder(modifier = Modifier.fillMaxSize())
+    else {
+        bitmap?.let {
+            Image(bitmap = it.asImageBitmap(), contentDescription = null)
+        }
     }
 
 }
 
+@Composable
+fun Placeholder(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.wrapContentSize(Alignment.Center)) {
+        Image(
+            painter = painterResource(R.drawable.no_image),
+            contentDescription = null,
+            modifier = Modifier.size(48.dp) // Adjust size as needed
+        )
+    }
+}
 
 suspend fun loadImageFromUrl(url: String): Bitmap? {
     var bitmap: Bitmap? = null
@@ -98,7 +118,6 @@ fun getUrlBytes(url: String): ByteArray? {
 
 
 suspend fun getBase64FromImageUrl(url: String): String? {
-
 
 
     return withContext(Dispatchers.IO) {
